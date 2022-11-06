e7a277c400119a018df7b080743abb1ad1506fc2
Denis Greckij <denisgreckij38@gmail.com>
2022-11-06 23:03:10 +0300

    feat: add RangeSlider class

--- online-store/src/components/customElements/FilterContainer/FilterCategoryContainer/FilterCategoryContainer.ts
+++ online-store/src/components/customElements/FilterContainer/FilterCategoryContainer/FilterCategoryContainer.ts
@@ -1,9 +1,24 @@
 import './FilterCategoryContainer.scss'
+import { RangeSlider } from './RangeSlider'
 
 export class FilterCategoryContainer extends HTMLElement {
+  constructor(){
+    super()
+    customElements.define('range-slider', RangeSlider)
+  }
+
   connectedCallback(){
+    const minSliderValue = 0
+    const maxSliderValue = 100
+    const firstValue = 1
+    const secondValue = 33
     this.innerHTML = `
-      <input type="range" is="range-slider">
+      <range-slider 
+        min=${minSliderValue}
+        max=${maxSliderValue}
+        firstValue=${firstValue}
+        secondValue=${secondValue}
+      ></range-slider>
     `
   }
 }
--- online-store/src/components/customElements/FilterContainer/FilterCategoryContainer/RangeSlider.ts
+++ online-store/src/components/customElements/FilterContainer/FilterCategoryContainer/RangeSlider.ts
@@ -1,7 +1,57 @@
 import './RangeSlider.scss'
 
-export class RangeSlider extends HTMLInputElement {
+export class RangeSlider extends HTMLElement {
+  private readonly minGap = 0
+
   connectedCallback(){
+    this.innerHTML = `
+    <div class="values">
+        <span data-id="range1">
+          ${this.getAttribute('firstValue')}
+        </span>
+        <span> - </span>
+        <span data-id="range2">
+          ${this.getAttribute('secondValue')}
+        </span>
+      </div>
+    <div class="slider-container">
+      <input
+        type="range"
+        data-id="slider1"
+        min=${this.getAttribute('min')}
+        max=${this.getAttribute('max')}
+        value=${this.getAttribute('firstValue')}
+      >
+      <input
+        type="range"
+        data-id="slider2"
+        min=${this.getAttribute('min')}
+        max=${this.getAttribute('max')}
+        value=${this.getAttribute('secondValue')}
+      >
+      <div data-id="sliderTrack" class="slider-track"></div>
+    </div>
+    `
+
+    const sliderOne: HTMLInputElement | null = this.querySelector('[data-id="slider1"]')
+    const sliderTwo: HTMLInputElement | null = this.querySelector('[data-id="slider2"]')
+    const rangeValueOne: HTMLElement | null = this.querySelector('[data-id="range1"]')
+    const rangeValueTwo: HTMLElement | null = this.querySelector('[data-id="range2"]')
     
+   if (sliderOne && sliderTwo) {
+    sliderOne.oninput = () => {
+      if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= this.minGap){
+        sliderOne.value = `${parseInt(sliderTwo.value) - this.minGap}`
+      }
+      if (rangeValueOne) rangeValueOne.textContent = sliderOne.value
+    }
+
+    sliderTwo.oninput = () => {
+      if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= this.minGap){
+        sliderOne.value = `${parseInt(sliderTwo.value) + this.minGap}`
+      }
+      if (rangeValueTwo) rangeValueTwo.textContent = sliderTwo.value
+    }
+   }
   }
 }
